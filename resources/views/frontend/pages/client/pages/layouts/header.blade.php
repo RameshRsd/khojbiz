eb77e19655e3284dfd3ef20d5a150a6a92c4f974
youramesh5@gmail.com <youramesh5@gmail.com>
2019-06-25 21:27:41 +0545

    client profile updated

--- resources/views/frontend/pages/client/pages/layouts/header.blade.php
+++ resources/views/frontend/pages/client/pages/layouts/header.blade.php
@@ -1,17 +1,17 @@
 <li>
-    <a class="nav-link d-flex justify-content-between align-items-center active" href="{{url($client->user->name.'/'.'about-us')}}">
+    <a class="nav-link d-flex justify-content-between align-items-center  @if(request()->segment('2') =='about-us') active @endif " href="{{url($client->user->name.'/'.'about-us')}}">
         About Us
     </a>
 </li>
 <li>
-    <a class="nav-link d-flex justify-content-between align-items-center" href="{{url($client->user->name.'/'.'profile')}}">Profile </a>
+    <a class="nav-link d-flex justify-content-between align-items-center  @if(request()->segment('2') =='profile') active @endif" href="{{url($client->user->name.'/'.'profile')}}">Profile </a>
 </li>
 <li>
-    <a class="nav-link d-flex justify-content-between align-items-center" href="{{url($client->user->name.'/'.'product-and-service')}}">Product & Service</a>
+    <a class="nav-link d-flex justify-content-between align-items-center @if(request()->segment('2') =='product-and-service') active @endif" href="{{url($client->user->name.'/'.'product-and-service')}}">Product & Service</a>
 </li>
 <li>
-    <a class="nav-link d-flex justify-content-between align-items-center" href="{{url($client->user->name.'/'.'image-gallery')}}">Image Gallery </a>
+    <a class="nav-link d-flex justify-content-between align-items-center @if(request()->segment('2') =='image-gallery') active @endif" href="{{url($client->user->name.'/'.'image-gallery')}}">Image Gallery </a>
 </li>
 <li>
-    <a class="nav-link d-flex justify-content-between align-items-center" href="{{url($client->user->name.'/'.'contact-us')}}">Contact Us </a>
+    <a class="nav-link d-flex justify-content-between align-items-center @if(request()->segment('2') =='contact-us') active @endif" href="{{url($client->user->name.'/'.'contact-us')}}">Contact Us </a>
 </li>
